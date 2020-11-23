592303f42e6e56e5b2693b39cb62623dd3eb7dd5
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-11-23 14:13:02 +0100

    added entrees to pom.xml

---
+++ src/main/java/be/vdab/AppStart.java
@@ -0,0 +1,13 @@
+package be.vdab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// The starting point of our Spring Boot Application
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class, args);
+    }
+
+}
